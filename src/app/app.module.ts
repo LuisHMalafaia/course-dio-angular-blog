41de925428375e249bf644db9108a838cf31a27c
Luis Henrique Malafaia <luishmalafaia@gmail.com>
2023-09-29 20:56:55 -0300

    create menu-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuTitleComponent } from './components/menu-title/menu-title.component
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SmallCardComponent } from './components/small-card/small-card.component
     MenuTitleComponent,
     BigCardComponent,
     HomeComponent,
-    SmallCardComponent
+    SmallCardComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
