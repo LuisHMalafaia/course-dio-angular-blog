e7cd8161bb2a55f5c24bce6d7deb9405d3e50ba0
Luis Henrique Malafaia <luishmalafaia@gmail.com>
2023-09-29 23:17:53 -0300

    create content page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BigCardComponent } from './components/big-card/big-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { ContentComponent } from './pages/content/content.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
     BigCardComponent,
     HomeComponent,
     SmallCardComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "https://res.cloudinary.com/practicaldev/image/fetch/s--bpx-IEW7--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/shcwa40hgffwns0d8jxy.png",
+        "title": "Angular News",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor dolor elit, vel consequat odio ullamcorper ac. In euismod mi dui, eget placerat lectus bibendum id. Phasellus vitae bibendum elit. Nam non lacus placerat, iaculis lacus vitae, egestas tellus."
+    },
+    {
+        "id": "2",
+        "photo": "https://miro.medium.com/v2/resize:fit:1000/1*1u2p917cvlyP6SuHcq3t0Q.jpeg",
+        "title": "HTML News",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor dolor elit, vel consequat odio ullamcorper ac. In euismod mi dui, eget placerat lectus bibendum id. Phasellus vitae bibendum elit. Nam non lacus placerat, iaculis lacus vitae, egestas tellus."
+    },
+    {
+        "id": "3",
+        "photo": "https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/1129786/retina_1708x683_01-Cover-87a7296050000bf3ac3694aee7fad051.png",
+        "title": "CSS News",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor dolor elit, vel consequat odio ullamcorper ac. In euismod mi dui, eget placerat lectus bibendum id. Phasellus vitae bibendum elit. Nam non lacus placerat, iaculis lacus vitae, egestas tellus."
+    },
+    {
+        "id": "4",
+        "photo": "https://www.educative.io/v2api/editorpage/5765104277651456/image/5194598537232384",
+        "title": "Typescript News",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor dolor elit, vel consequat odio ullamcorper ac. In euismod mi dui, eget placerat lectus bibendum id. Phasellus vitae bibendum elit. Nam non lacus placerat, iaculis lacus vitae, egestas tellus."
+    }
+]
+
